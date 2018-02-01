8c38949062046f1a6651522ae5f08c353491345e
deepnav1256 <deepnav1256@gmail.com>
2018-02-01 13:37:52 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,TemplateRef,EventEmitter,ViewChild } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Route } from "@angular/router";
 
+const routes: Route[] = [{
+
+}];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  {
+   constructor() { };;
+  
 }
+    
+  
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from "@angular/forms";
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { RouterModule, Route } from "@angular/router";
+import{ UserService} from './user.service'
+import{ AuthguardGuard} from './authguard.guard'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    GamesComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule,
+    ModalModule.forRoot(),
+    RouterModule.forRoot([{
+     path:'',
+     redirectTo:'home',
+     pathMatch:'full' 
+    },
+  {
+    path:'home',
+    canActivate:[AuthguardGuard],
+    component:HomeComponent
+  },
+{path:'games',
+canActivate:[AuthguardGuard],
+component:GamesComponent
+
+},
+{
+  path:'login',
+  component:LoginComponent
+}
+])
+],
+  providers: [UserService,AuthguardGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
